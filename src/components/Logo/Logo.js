b4984b359edfdff37a222b317d333edd44ad994e
Muhammed Nazir <mnazir9876@gmail.com>
2020-09-02 15:26:50 -0500

    toolbar implemented

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import burgerLogo from '../../assets/images/burger-logo.png';
+import classes from './Logo.module.css';
+
 
 const logo = (props) => (
-    <div>
-        
+    <div className={classes.Logo}>
+        <img src={burgerLogo} alt="MyBurger" />
 
     </div>
 );
