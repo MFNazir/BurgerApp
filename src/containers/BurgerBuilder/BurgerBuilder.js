b1096f93e315ca15bc2adac0bd9875e61948f871
Muhammed Nazir <mnazir9876@gmail.com>
2020-09-14 23:38:28 -0500

    orders works now and full routing

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -115,11 +115,12 @@ class BurgerBuilder extends Component {
                     for (let i in this.state.ingredients) {
                         queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
                     }
+                    queryParams.push('price=' + this.state.totalPrice);
                     const queryString = queryParams.join('&');
                     this.props.history.push({
-                        pathname: '/checkout', //this sends you to the checkout page
+                        pathname: '/checkout',
                         search: '?' + queryString
-                    });
+                });
     }
 
     render() {
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -8,22 +8,23 @@ import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
     state = {
-        ingredients: {
-            salad: 1,
-            meat: 1,
-            cheese: 1,
-            bacon: 1
-        }
+        ingredients: null,
+        price: 0
     }
 
-    componentDidMount() {
-        const query = new URLSearchParams(this.props.location.search);
+    componentWillMount () {
+        const query = new URLSearchParams( this.props.location.search );
         const ingredients = {};
-        for (let param of query.entries()) {
+        let price = 0;
+        for ( let param of query.entries() ) {
             // ['salad', '1']
-            ingredients[param[0]] = +param[1];
+            if (param[0] === 'price') {
+                price = param[1];
+            } else {
+                ingredients[param[0]] = +param[1];
+            }
         }
-        this.setState({ingredients: ingredients});
+        this.setState( { ingredients: ingredients, totalPrice: price } );
     }
 
     checkoutCancelledHandler = () => {
@@ -34,16 +35,20 @@ class Checkout extends Component {
         this.props.history.replace('/checkout/contact-data');
     }
 
+    
     render() {
         return (
+            //props in render gives you the history 
             <div>
                  <CheckoutSummary 
                     ingredients={this.state.ingredients}
                     checkoutCancelled={this.checkoutCancelledHandler}
                     checkoutContinued={this.checkoutContinuedHandler}/>
+                    
                     <Route 
                         path={this.props.match.path + '/contact-data'} 
-                        component={ContactData} />
+                        render={(props) => (<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props} />)} /> 
+                        
             </div>
         );
     }
